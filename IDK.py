2e0971d6c219122ca34e985129cafdbac25ea071
ykip10 <nisaypik@gmail.com>
2024-07-03 00:46:47 +1000

    Update IDK.py

--- IDK.py
+++ IDK.py
@@ -1,2 +1,12 @@
 print("hello cookies")
 # TO COOKIES: print SOMETHING BELOW THIS LINE TO TEST IF I GET YOUR UPDATES 
+
+import pathlib
+from pathlib import Path 
+import sys
+
+venv_path = Path(r'C:\Users\YKip1\OneDrive\Desktop\cookies\.venv\Lib\site-packages')
+sys.path.insert(0, str(venv_path))
+print(f'Path: {pathlib.Path(__file__).parent.resolve()}')
+print(f'Path: {pathlib.Path().resolve()}')
+print(sys.version) 

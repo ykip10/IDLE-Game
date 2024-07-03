4faef15d227b98441e6cd22f17b0d6632cac6218
ykip10 <nisaypik@gmail.com>
2024-07-04 03:52:34 +1000

    Update IDK.py

--- IDK.py
+++ IDK.py
@@ -22,11 +22,13 @@ def x(x):
     """
     return width * x / NATIVE_WIDTH
 
+
 def y(y):
     """
     Returns y-coordinate scaled to custom resolutions
     """
     return height * y / NATIVE_HEIGHT
+
 
 #Resources Class
 class Resource:
@@ -87,6 +89,7 @@ class Generator:
     def get_gen(id_number):
         return Generator.id_to_generator.get(id_number)
 
+
 #Resource Types
 money = Resource("Money", 1)                            #Standard resource to buy upgrades
 gems = Resource("Gems", 0)                              #Rare & Premium currency
@@ -103,7 +106,8 @@ def buy_producer(producer, cost, rate_increase):
     if money.amount >= cost:                            #checks if player can purchase generator
         money.amount -= cost                            #deducts cost if player can purchase generator
         producer.rate += rate_increase                  #increases production of generator by the rate increase
-        
+
+      
 # Font
 font = pygame.font.Font(None, 36)
 
@@ -113,42 +117,22 @@ def draw_text(surface, text, x, y):                     #surface = screen it dra
     surface.blit(text_surface, (x, y))
 
 
-
-#Main Game Loop
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = event.pos
-            if width * 600/1280 <= mouse_x <= width and 0 <= mouse_y <= height:
-                for i in range(Resource.id_no):                                      
-                    Resource.get_resource(i).add(Resource.get_resource(i).click_rate)
-            elif x(15) <= mouse_x <= x(200) and y(100) <= mouse_y <= y(130):      #Purchase generator 1
-                generator1.buy()
-            elif x(15) <= mouse_x <= x(200) and y(150) <= mouse_y <= y(180):   #Purchase generator 2
-                generator2.buy()
-
-    
-    #Update Resource Generators and get total rate
-    total_rate = 0
-    for i in range(Generator.id_no):                                      
-        Generator.get_gen(i).update()
-        total_rate += Generator.get_gen(i).rate
-
-    #Fill screen with black
-    screen.fill(BLACK)
-
-    #Draw Visuals & Buttons
-    draw_text(screen, str(money), x(10), y(10))                           #Current Resource Amount
+# Changes scenes to main screen 
+def show_mainscreen():
+    """
+    Draws Visuals & Buttons for the main screen 
+    """
+    # Resources info
+    draw_text(screen, str(money), x(10), y(10))                           # Current Resource Amount
     draw_text(screen, str(gems), x(200), y(10))                           # Gem count
     draw_text(screen, f'Income: {str(total_rate)}g/s', x(390), y(10))     # Current income 
 
-    pygame.draw.rect(screen, (0, 128, 0), (x(600), y(0), x(720), y(720)))      #Clicking area
-    pygame.draw.rect(screen, (0, 128, 0), (x(15), y(100), x(200), y(30)))      #screen, RGB, position(x1,y1,x2,y2)
+    # Clicking area 
+    pygame.draw.rect(screen, (0, 128, 0), (x(600), y(0), x(720), y(720)))     
 
-    # Visuals associated to generators 
+
+    # Generators information
+    pygame.draw.rect(screen, (0, 128, 0), (x(15), y(100), x(200), y(30)))      # screen, RGB, position(x1,y1,x2,y2)
     draw_text(screen, "Buy Generator 1", x(20), y(100))
     draw_text(screen, '+' + str(generator1.base_rate) + ' g/s', x(225), y(100))
     draw_text(screen, 'Current: ' + str(generator1.rate) + 'g/s', x(315), y(100))
@@ -156,10 +140,65 @@ while run:
     draw_text(screen, "Buy Generator 2", x(20), y(150))
     draw_text(screen, '+' + str(generator2.base_rate) + ' g/s', x(225),  y(150))
     draw_text(screen, 'Current: ' + str(generator2.rate) + 'g/s', x(315), y(150))
-    
+
+    # Shop button
+    pygame.draw.rect(screen, (0, 128, 0), (x(15), y(670), x(80), y(30)))
+    draw_text(screen, "Shop", x(20), y(670))
+
+
+# Changes scenes to shop screen 
+def show_shopscreen():
+    screen.fill(BLACK)
+
+    # Return button
+    pygame.draw.rect(screen, (0, 128, 0), (x(15), y(670), x(90), y(30)))
+    draw_text(screen, "Return", x(20), y(670))
+
+#Main Game Loop
+
+# Screens 
+main_screen = True
+shop_screen = False
+
+run = True
+while run:
+    for event in pygame.event.get():
+        #Quit Pygame
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+
+            # Clicking for resources 
+            if x(600) <= mouse_x <= x(width) and y(0) <= mouse_y <= y(height):
+                for i in range(Resource.id_no):                                      
+                    Resource.get_resource(i).add(Resource.get_resource(i).click_rate)
+            # Buy Generator Buttons
+            elif x(15) <= mouse_x <= x(200) and y(100) <= mouse_y <= y(130):      #Purchase generator 1
+                generator1.buy()
+            elif x(15) <= mouse_x <= x(200) and y(150) <= mouse_y <= y(180):   #Purchase generator 2
+                generator2.buy()
+            # Shop button 
+            elif x(15) <= mouse_x <= x(90) and y(670) <= mouse_y <= y(700):
+                main_screen = False
+                shop_screen = True
+    #Update Resource Generators and get total rate
+    total_rate = 0
+
+    for i in range(Generator.id_no):                                      
+        Generator.get_gen(i).update()
+        total_rate += Generator.get_gen(i).rate
+
+    #Fill screen with black
+    screen.fill(BLACK)
+
+    if main_screen:
+        show_mainscreen()
+    elif shop_screen:
+        show_shopscreen()
 
     #Update Display
     pygame.display.flip()
-#Quit Pygame
-pygame.quit()
-sys.exit()
+
+

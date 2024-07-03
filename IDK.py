e414b905a34f8db0a8c68eab0e79eec4db7d8500
ykip10 <nisaypik@gmail.com>
2024-07-03 16:52:45 +1000

    whole lotta nothing

--- IDK.py
+++ IDK.py
@@ -1,14 +1,38 @@
-import pygame
+import pygame, sys 
+import numpy as np 
 
+pygame.init()
 
+FPS = 30
+BLACK = 0, 0, 0
+
+clock = pygame.time.Clock()
 #Game Window
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+terrain = pygame.image.load(r'sprites\grassy terrain.jpg')
+terrain = pygame. transform.scale(terrain, (1280, 720))
+terrainrect = terrain.get_rect()
+ball = pygame.image.load(r'sprites\ball.jpg')
+ballrect = ball.get_rect()
+ballrect = ballrect.move(500, 200)
+
+velocity = [0, 10] # initial speed
+acceleration = [0, -0.098] # acceleration
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    ballrect = ballrect.move(velocity)
+    velocity = np.add(velocity, acceleration)
+
+    screen.blit(terrain, terrainrect)
+    screen.blit(ball, ballrect)
+    clock.tick(FPS)
+    pygame.display.flip() 
 pygame.quit()

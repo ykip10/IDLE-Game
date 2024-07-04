59ac74c89cf8f2e81a19f889c926715c33a45fd2
ykip10 <nisaypik@gmail.com>
2024-07-04 17:19:17 +1000

    PARTITIONED INTO MORE FILES
    scene_change.py needs work and main.py doesn't work because there is unresolved reference to objects.py for some reason (probably because of circular imports on line 55 of objects.py)

--- main.py
+++ main.py
@@ -1,103 +1,41 @@
-import pygame, sys 
+import pygame, sys, settings, objects
 import numpy as np 
 
 pygame.init()
 
-FPS = 30
 NATIVE_WIDTH = 1280
 NATIVE_HEIGHT = 720
-#Colours
+# Colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 clock = pygame.time.Clock()
 
-#Screen Settings
-width = 1280
-height = 720
-screen = pygame.display.set_mode((width, height))
+# Settings
+fps = settings.fps
+screen = pygame.display.set_mode((settings.width, settings.height))
 
 def x(x):
     """
     Returns x-coordinate scaled to custom resolutions
     """
-    return width * x / NATIVE_WIDTH
+    return settings.width * x / NATIVE_WIDTH
 
 
 def y(y):
     """
     Returns y-coordinate scaled to custom resolutions
     """
-    return height * y / NATIVE_HEIGHT
-
-
-#Resources Class
-class Resource:
-    id_no = 0
-    id_list = []
-    id_to_resource = dict()
-    def __init__(self, name, click_rate):
-        self.name = name                                #Name of Resource
-        self.amount = 0                                 #Resource Amount
-        self.click_rate = click_rate
-        self.id = Resource.id_no
-        Resource.id_no += 1
-        Resource.id_list.append(self.id)
-        Resource.id_to_resource.update({self.id:self})
-
-    def add(self, amount):
-        self.amount += amount                           # Increase resource value by "amount"
-
-    def __str__(self):
-        return f"{self.name}: {self.amount}"            #Returns resource name and amount
-    
-    def purchasable(self,price):
-        return self.amount >= price
-
-    def get_resource(id_number):
-        return Resource.id_to_resource.get(id_number)
-        
-
-# Generator class                                    py
-class Generator:                                     
-    id_no = 0
-    id_list = []
-    id_to_generator = dict()
-    def __init__(self, name, resource, base_rate,cost):       
-        self.name = name                                #Name of Generator
-        self.resource = resource                        #Resource Generator makes      
-        self.base_rate = base_rate
-        self.rate = 0                                #Resource generation rate
-        self.last_update = pygame.time.get_ticks()      #Stores time since last update (ms)
-        self.cost = cost
-        self.id = Generator.id_no
-        Generator.id_no += 1
-        Generator.id_list.append(self.id)
-        Generator.id_to_generator.update({self.id:self})
-
-    def update(self):
-        now = pygame.time.get_ticks()
-        elapsed = now - self.last_update
-        if elapsed > 1000:                              # Update every second
-            self.resource.add(self.rate)
-            self.last_update = now
-
-    def buy(self):
-        if self.resource.purchasable(self.cost):                            #checks if player can purchase generator
-            money.amount -= self.cost                            #deducts cost if player can purchase generator
-            self.rate += self.base_rate                  #increases production of generator by the rate increase ##
-
-    def get_gen(id_number):
-        return Generator.id_to_generator.get(id_number)
+    return settings.height * y / NATIVE_HEIGHT
 
 
 #Resource Types
-money = Resource("Money", 1)                            #Standard resource to buy upgrades
-gems = Resource("Gems", 0)                              #Rare & Premium currency
+money = objects.Resource("Money", 1)                            #Standard resource to buy upgrades
+gems = objects.Resource("Gems", 0)                              #Rare & Premium currency
 
 #Resource Generator                                           
-generator1 = Generator("generator1", money, 1,10)          #tier 1 generator
-generator2 = Generator("generator2", money, 5,100)          #tier 2 generator
-generator3 = Generator("generator3", money, 20,1000)         #tier 3 generator
+generator1 = objects.Generator("generator1", money, 1,10)          #tier 1 generator
+generator2 = objects.Generator("generator2", money, 5,100)          #tier 2 generator
+generator3 = objects.Generator("generator3", money, 20,1000)         #tier 3 generator
       
 # Font
 font = pygame.font.Font(None, 36)
@@ -107,12 +45,15 @@ def draw_text(surface, text, x, y):                     #surface = screen it dra
     text_surface = font.render(text, True, WHITE)       
     surface.blit(text_surface, (x, y))
 
+def buy(object_name, resource):
+    if object_name.
 
 # Changes scenes to main screen 
 def show_mainscreen():
     """
     Draws Visuals & Buttons for the main screen 
     """
+
     # Resources info
     draw_text(screen, str(money), x(10), y(10))                           # Current Resource Amount
     draw_text(screen, str(gems), x(200), y(10))                           # Gem count
@@ -120,7 +61,6 @@ def show_mainscreen():
 
     # Clicking area 
     pygame.draw.rect(screen, (0, 128, 0), (x(600), y(0), x(720), y(720)))     
-
 
     # Generators information
     pygame.draw.rect(screen, (0, 128, 0), (x(15), y(100), x(200), y(30)))      # screen, RGB, position(x1,y1,x2,y2)
@@ -145,13 +85,13 @@ def show_shopscreen():
     pygame.draw.rect(screen, (0, 128, 0), (x(15), y(670), x(90), y(30)))
     draw_text(screen, "Return", x(20), y(670))
 
-#Main Game Loop
 
 # Screens 
 main_screen = True
 shop_screen = False
 
-run = True
+#Main Game Loop
+run = True  
 while run:
     for event in pygame.event.get():
         #Quit Pygame
@@ -163,8 +103,8 @@ while run:
 
             # Clicking for resources 
             if x(600) <= mouse_x <= x(width) and y(0) <= mouse_y <= y(height):
-                for i in range(Resource.id_no):                                      
-                    Resource.get_resource(i).add(Resource.get_resource(i).click_rate)
+                for i in range(objects.Resource.id_no):                                      
+                    objects.Resource.get_resource(i).add(objects.Resource.get_resource(i).click_rate)
             # Buy Generator Buttons
             elif x(15) <= mouse_x <= x(200) and y(100) <= mouse_y <= y(130):      #Purchase generator 1
                 generator1.buy()
@@ -178,8 +118,8 @@ while run:
     total_rate = 0
 
     for i in range(Generator.id_no):                                      
-        Generator.get_gen(i).update()
-        total_rate += Generator.get_gen(i).rate
+        objects.Generator.get_gen(i).update()
+        total_rate += objects.Generator.get_gen(i).rate
 
     #Fill screen with black
     screen.fill(BLACK)

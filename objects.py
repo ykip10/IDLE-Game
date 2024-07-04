ece8a61533fdfa27cf92bd49c8c18d16652021d5
wontonyep <160245743+wontonyep@users.noreply.github.com>
2024-07-04 18:18:04 +1000

    el wonton special
    kinda like whats going on here interesting ideas good changing of formatting and shit fixed up main.py and the importing bullshit deal

---
+++ objects.py
@@ -0,0 +1,60 @@
+import pygame
+# Resources Class
+class Resource:
+    id_no = 0
+    id_list = []
+    id_to_resource = dict()
+    def __init__(self, name, click_rate):
+        self.name = name                                # Name of Resource
+        self.amount = 0                                 # Resource Amount
+        self.click_rate = click_rate
+        self.id = Resource.id_no
+        Resource.id_no += 1
+        Resource.id_list.append(self.id)
+        Resource.id_to_resource.update({self.id:self})
+
+    def add(self, amount):
+        self.amount += amount                           # Increase resource value by "amount"
+
+    def __str__(self):
+        return f"{self.name}: {self.amount}"            # Returns resource name and amount
+    
+    def purchasable(self,price):
+        return self.amount >= price
+
+    def get_resource(id_number):
+        return Resource.id_to_resource.get(id_number)
+    
+# Generator class
+class Generator:                                     
+    id_no = 0
+    id_list = []
+    id_to_generator = dict()
+    def __init__(self, name, resource, base_rate,cost):       
+        self.name = name                                #Name of Generator
+        self.resource = resource                        #Resource Generator makes      
+        self.base_rate = base_rate
+        self.rate = 0                                #Resource generation rate
+        self.last_update = pygame.time.get_ticks()      #Stores time since last update (ms)
+        self.cost = cost
+        self.id = Generator.id_no
+        Generator.id_no += 1
+        Generator.id_list.append(self.id)
+        Generator.id_to_generator.update({self.id:self})
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        elapsed = now - self.last_update
+        if elapsed > 1000:                              # Update every second
+            self.resource.add(self.rate)
+            self.last_update = now
+
+    def buy(self):
+        if self.resource.purchasable(self.cost):                            #checks if player can purchase generator
+            self.resource.amount -= self.cost                            #deducts cost if player can purchase generator
+            self.rate += self.base_rate                  #increases production of generator by the rate increase ##
+
+    def get_gen(id_number):
+        return Generator.id_to_generator.get(id_number)
+
+        
---
+++ scene_change.py
@@ -0,0 +1,64 @@
+import pygame, sys, settings
+
+# Need something that runs the loop, something that executes specific commands for each scene. 
+
+# While True:
+#    engine(Scene)
+#        
+#
+#
+#
+
+class Transition:
+    def __init__(self):
+        self.current = None
+        self.next_scene = None
+
+    def update(self):
+        if self.next_scene:
+            self.current = self.next_scene
+            self.next_scene = None
+
+class DisplayEngine:
+    def __init__(self):
+        self.fps = settings.fps
+        self.surface = pygame.display.set_mode((settings.width, settings.height))
+        self.clock = pygame.time.Clock()
+        self.delta = 0 
+        self.Transition = Transition()
+
+    def loop(self):
+        while True:
+            for event in pygame.event.get():
+            #Quit Pygame
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                else:
+                    self.Transition.current.on_event(event)
+            
+            self.Transition.current.draw(self.surface)
+
+class Scene: 
+    def __init__(self): pass
+    def draw(self, surface): pass
+    def on_event(self, event): pass 
+        
+class main_scene(Scene):
+    def __init__(self, background):
+        self.background = background
+    def draw(self, surface):
+        self.surface = surface
+    def on_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            # Clicking for resources 
+            if x(600) <= mouse_x <= x(width) and y(0) <= mouse_y <= y(height):
+                for i in range(Resource.id_no):                                      
+                    Resource.get_resource(i).add(Resource.get_resource(i).click_rate)
+            # Buy Generator Buttons
+            elif x(15) <= mouse_x <= x(200) and y(100) <= mouse_y <= y(130):      #Purchase generator 1
+                generator1.buy()
+            elif x(15) <= mouse_x <= x(200) and y(150) <= mouse_y <= y(180):   #Purchase generator 2
+                generator2.buy()
+        
